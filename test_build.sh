6edad2d5d2b9e89ce5232053e6119fde52260c03
grey <you@example.com>
2017-04-28 11:28:31 +0200

    initial commit for running in travic

---
+++ test_build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# export SCRAM_ARCH=slc6_amd64_gcc481
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+
+mkdir -p /home/build && cd /home/build
+
+echo "# ================= #"
+echo "# cmsset_default.sh"
+echo "# ================= #"
+cat $VO_CMS_SW_DIR/cmsset_default.sh
+echo "# ================= #"
+. $VO_CMS_SW_DIR/cmsset_default.sh
+
+scram project ${TEST_CMSSW_VERSION}
+
+cd ${TEST_CMSSW_VERSION}
+
+cmsenv
+
+cd src/
+
+echo "# ================= #"
+echo "# Building in CMSSW #"
+echo "# ================= #"
+
+scram b -v -j 2 || exit 1
