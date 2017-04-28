9b097e38e01e6e355a0c203ab4a1a1653a57909b
grey <you@example.com>
2017-04-28 11:40:02 +0200

    simplified test.sh

--- test_build.sh
+++ test_build.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
+echo "yes"
+# # export SCRAM_ARCH=slc6_amd64_gcc481
+# export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 
-# export SCRAM_ARCH=slc6_amd64_gcc481
-export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+# mkdir -p /home/build && cd /home/build
 
-mkdir -p /home/build && cd /home/build
+# echo "# ================= #"
+# echo "# cmsset_default.sh"
+# echo "# ================= #"
+# cat $VO_CMS_SW_DIR/cmsset_default.sh
+# echo "# ================= #"
+# . $VO_CMS_SW_DIR/cmsset_default.sh
 
-echo "# ================= #"
-echo "# cmsset_default.sh"
-echo "# ================= #"
-cat $VO_CMS_SW_DIR/cmsset_default.sh
-echo "# ================= #"
-. $VO_CMS_SW_DIR/cmsset_default.sh
+# scram project ${TEST_CMSSW_VERSION}
 
-scram project ${TEST_CMSSW_VERSION}
+# cd ${TEST_CMSSW_VERSION}
 
-cd ${TEST_CMSSW_VERSION}
+# cmsenv
 
-cmsenv
+# cd src/
 
-cd src/
+# echo "# ================= #"
+# echo "# Building in CMSSW only #"
+# echo "# ================= #"
 
-echo "# ================= #"
-echo "# Building in CMSSW only #"
-echo "# ================= #"
-
-scram b -v -j 2 || exit 1
+# scram b -v -j 2 || exit 1
